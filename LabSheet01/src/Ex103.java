361a17b19d6945fd466aa1dccc2bb0a78e5c64cc
Oilkc56 <Kittinanthat146@gmail.com>
2025-11-18 13:42:18 +0700

    submit exercise LabSheet01

---
+++ LabSheet01/src/Ex103.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Ex103 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "I am Kittinant thatpaibool",
+                "Welcome",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
