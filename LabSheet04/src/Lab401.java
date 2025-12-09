ee69ba97728bf0a193e9139e1d57c8bac95b5b58
Oilkc56 <kittinanthat146@gmail.com>
2025-12-09 14:15:38 +0700

    Kittinant LabSheet04

---
+++ LabSheet04/src/Lab401.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Lab401 {
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your full name:");
+
+        int index = name.indexOf(" ");
+
+        while (name.trim().indexOf(" ") == -1){
+            name = JOptionPane.showInputDialog("Enter your full name:");
+
+        }
+        JOptionPane.showMessageDialog(null,"Welcome, " + name.toUpperCase().charAt(0) +
+                name.substring(1, index) +" "+ name.toUpperCase().substring(index+1,name.length()-1));
+
+
+    }
+
+}
